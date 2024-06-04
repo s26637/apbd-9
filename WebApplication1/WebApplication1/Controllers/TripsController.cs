25a92fdff230879514a507c64eaf5e7f35d42f62
s26637 <s26637@pjwst.edu.pl>
2024-06-04 14:07:28 +0200

    add

---
+++ WebApplication1/WebApplication1/Controllers/TripsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+
+namespace WebApplication1.Controllers;
+
+
+
+
+[ApiController]
+[Route("api/[controller]")]
+public class TripsController : ControllerBase
+{
+    private readonly ApbdContext _context;
+    public TripsController(ApbdContext context)
+    {
+        _context = context;
+    }
+    
+    [HttpGet]
+    public async Task<IActionResult> GetTrips()
+    {
+        var trips = await _context.Trips.ToListAsync();
+        return Ok(trips);
+    }
+}
